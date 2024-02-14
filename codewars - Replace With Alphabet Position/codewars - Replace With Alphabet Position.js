144b1e87c7e012ba4bfeb0ef9a1b8d96bc9ff1b6
ShaharAshe <shaharas30@gmail.com>
2024-02-14 11:21:40 +0200

    alphabetה position - code

---
+++ codewars - Replace With Alphabet Position/codewars - Replace With Alphabet Position.js
@@ -0,0 +1,10 @@
+function alphabetPosition(text) {
+    const a_ascii = "a".charCodeAt();
+    const z_ascii = "z".charCodeAt();
+    const space_ascii = " ".charCodeAt();
+    return text.toLowerCase().split("").map(word => (word.charCodeAt() >= a_ascii && word.charCodeAt() <= z_ascii)? word.charCodeAt()-a_ascii+1 : "").filter(Boolean).join(" ");
+}
+
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")) // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"));
+console.log(alphabetPosition("The narwhal bacons at midnight.")) // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"));
